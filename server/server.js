3e94b6e91596928d27ea7b5e6bf032738b4339fb
Enzo <enzod98@hotmail.com>
2020-04-09 16:07:51 -0400

    Arreglado server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extend: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
