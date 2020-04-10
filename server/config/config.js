fe9cf56e31d26d485b7adf3c124ec8406d50cd44
Enzo <enzod98@hotmail.com>
2020-04-10 18:36:31 -0400

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ process.env.PORT = process.env.PORT || 3000
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 
-let urlDB = 'mongodb+srv://esno:4ydIxbW6bvvel4uN@cluster0-kiv9c.mongodb.net/cafe'
+let urlDB = process.env.MONGO_URL;
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
